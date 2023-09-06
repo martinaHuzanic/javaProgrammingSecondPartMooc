95e98819fc27c6864dd3ca3b449f9439ca39b0b7
martinaHuzanic <143994576+martinaHuzanic@users.noreply.github.com>
2023-09-06 13:26:24 +0200

    Update MainProgram.java

--- part10-Part10_15.RegularExpressions/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 public class MainProgram {
 
     public static void main(String[] args) {
-        // you can create test code here. Call the methods that you'll implement
-        // during the course of this exercise
         Scanner scan = new Scanner(System.in);
         Checker check = new Checker();
         while (true) {
